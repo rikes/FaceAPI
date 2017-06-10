65e73f2ecafeac73adac83d050f541d308611e22
rikes <henricke.ss@gmail.com>
2017-06-09 22:34:37 -0300

    Projeto via cmd e universal app

---
+++ ClienteSimples/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Net.Http;
+
+namespace ClienteSimples
+{
+    class Program
+    {
+        
+
+        static void Main()
+        {
+            string emotionKey = "53349a89ad7148cba0590fc965a39d96";
+            string faceKey = "f3cc67c6452240b58d000c1854b1ff98";
+
+
+            Console.Write("Enter the path to the JPEG image with faces to identify:");
+            string imageFilePath = @"C:\Users\henri\Downloads\expressoes.jpg";
+            
+            Console.WriteLine("Caminho {0}", imageFilePath);
+            Console.WriteLine("Arquivo {0}",Path.GetFileName(imageFilePath));
+
+            MakeDetectRequest(imageFilePath, emotionKey);
+
+            Console.WriteLine("\n\n\nWait for the result below, then hit ENTER to exit...\n\n\n");
+            Console.ReadLine();
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        static async void MakeDetectRequest(string imageFilePath, string key)
+        {
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid key.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+            // Request parameters for FAce API
+            //string queryString = "returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,smile,facialHair,glasses";
+
+            // NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+            //   For example, if you obtained your subscription keys from westus, replace "westcentralus" in the 
+            //   URI below with "westus".
+            //string uri = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?" + queryString;
+
+            //Url APi Emotion
+            string uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?";
+
+
+            HttpResponseMessage response;
+            string responseContent;
+
+            // Request body. Try this sample with a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+                responseContent = response.Content.ReadAsStringAsync().Result;
+            }
+
+            //A peak at the JSON response.
+            Console.WriteLine(responseContent);
+        }
+    }
+}
---
+++ ReconhecimentoDeSentimentos/MainPage.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Media.Capture;//Camera
+using Windows.Storage;//Manipulacao do arquivo
+using Windows.Storage.Streams;
+using Windows.Graphics;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
+
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+
+using Windows.Storage.Pickers;
+
+namespace ReconhecimentoDeSentimentos
+{
+    public sealed partial class MainPage : Page
+    {
+        CameraCaptureUI captureUI = new CameraCaptureUI();
+        StorageFile photo;
+        IRandomAccessStream imageStream;
+
+        const string emotionKey = "53349a89ad7148cba0590fc965a39d96";
+        EmotionServiceClient emotionService = new EmotionServiceClient(emotionKey);
+        Emotion[] emotionResults;//É um array de json pois identificara todos os rostos
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            this.captureUI.PhotoSettings.CroppedSizeInPixels = new Size(250, 200);
+
+
+        }
+        /*
+         *Metodo responsavel por capturar uma imagem e enviar a MainPage
+         * Mais informações: https://docs.microsoft.com/pt-br/windows/uwp/audio-video-camera/capture-photos-and-video-with-cameracaptureui
+         */
+        private async void take_photo(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Captura assincrona
+                photo = await this.captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                
+                // Se o Usuario cancelou a captura da foto
+                if (photo == null)
+                {
+                    
+                    return;
+                }
+                else
+                {
+                    //Carrego a foto
+                    this.imageStream = await photo.OpenAsync(FileAccessMode.Read);
+                    BitmapDecoder decoder = await BitmapDecoder.CreateAsync(imageStream);
+                    SoftwareBitmap softBitmap = await decoder.GetSoftwareBitmapAsync();
+                    
+                    //Converto com as exigencias de exibição na pagina XAML
+                    SoftwareBitmap softBitmapBGR8 = SoftwareBitmap.Convert(softBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                    SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+                    await bitmapSource.SetBitmapAsync(softBitmapBGR8);
+                    
+                    //Anexo ao campo "image" a foto armazenada
+                    image.Source = bitmapSource;
+                }
+
+
+            }
+            catch
+            {
+                //Envio a mensagem de erro pelo campo text que criei na tela
+                output.Text = "Erro: taking photo";   
+            }
+         }
+
+        private async void getEmotion_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                emotionResults = await emotionService.RecognizeAsync(imageStream.AsStream());
+                
+                if (emotionResults != null)
+                {
+                    //Para testes só usurei um, mas poderia haver mais;
+                    int i = 0;
+                    foreach (var p in emotionResults)
+                    {
+                        
+                        var score = p.Scores;
+                        output.Text += "Your Emotions are for photo #" +i+ "  : \n" +
+
+                         "Happiness: " + String.Format("{0:0.##}", score.Happiness * 100) + " %" + "\n" +
+
+                         "Sadness: " + String.Format("{0:0.##}", score.Sadness * 100) + " %" + "\n" +
+
+                         "Surprise: " + String.Format("{0:0.##}", score.Surprise * 100) + " %" + "\n" +
+
+                         "Anger: " + String.Format("{0:0.##}", score.Anger * 100) + " %" + "\n" +
+
+                         "Contempt: " + String.Format("{0:0.##}", score.Contempt * 100) + " %" + "\n" +
+
+                         "Disgust: " + String.Format("{0:0.##}", score.Disgust * 100) + " %" + "\n" +
+
+                         "Fear: " + String.Format("{0:0.##}", score.Fear * 100) + " %" + "\n" +
+
+                         "Neutral: " + String.Format("{0:0.##}", score.Neutral * 100) + " %" + "\n";
+                       i++;
+                    }
+                    
+
+                }
+            }
+            catch(Exception ex)
+            {
+                output.Text = "Erro: Check Emotions \n" + ex.Message + "\n";
+            }
+
+
+        }
+
+        private async void getPhotoLocal_Click(object sender, RoutedEventArgs e)
+        {
+
+            FileOpenPicker open = new FileOpenPicker();
+            open.FileTypeFilter.Add(".jpg");
+            open.FileTypeFilter.Add(".jpeg");
+            // Open a stream for the selected file 
+            StorageFile file = await open.PickSingleFileAsync();
+            
+            // Ensure a file was selected 
+            if (file != null)
+            {
+                imageStream = await file.OpenAsync(FileAccessMode.Read);
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(imageStream);
+                SoftwareBitmap softBitmap = await decoder.GetSoftwareBitmapAsync();
+                
+                //Converto com as exigencias de exibição na pagina XAML
+                SoftwareBitmap softBitmapBGR8 = SoftwareBitmap.Convert(softBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+                await bitmapSource.SetBitmapAsync(softBitmapBGR8);
+
+                //Anexo ao campo "image"
+                image.Source = bitmapSource;
+
+                /*
+                // Set the image source to the selected bitmap 
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.DecodePixelWidth = 600; //match the target Image.Width, not shown
+                await bitmapImage.SetSourceAsync(fileStream);
+                //Scenario2Image.Source = bitmapImage;
+                image.Source = bitmapImage; */
+
+            }
+            else
+            {
+                output.Text = "Cancelou o envio de imagem";
+            }
+        }
+    }
+}
